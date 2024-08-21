2de7fdd5db9336f67467f10888b0e76906340dfd
rameen-danish <rameenrashid15@gmail.com>
2024-08-22 04:48:27 +0500

    creating hello world

---
+++ helloworld/src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main>
+      <div className="all">
+      <h1 className="heading">Hello World</h1>
+      <h2 className="sub-heading">My first Next.JS program</h2>
+      <h2 className="head">Created by Rameen Danish</h2>
+      </div>
+    </main>
+  );
+}

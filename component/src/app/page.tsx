493f82228b39ba121a47d95c2b20498b8f554827
rameen-danish <rameenrashid15@gmail.com>
2024-08-23 04:43:56 +0500

    Making another component

--- component/src/app/page.tsx
+++ component/src/app/page.tsx
@@ -1,19 +1,31 @@
 
 export default function Home() {
+  // Making Component
+  const Greetings = (props:any)=>{
+    return(
+      <div>
+        <h2>Hello {props.name}! , Welcome to our coding world</h2>
+      </div>
+    )
+  }
+  // Making another component
+  const Message = ()=>{
+    return(
+      <h2>GIAIC Senior Students</h2>
+    )
+  }
   return (
     <main>
+      {/*  calling component  */}
       <Greetings name="Rameen"/>          
       <Greetings name="Abeeha"/>
       <Greetings name="Bismah"/>
       <Greetings name="Zaeema"/>
+      <Message/>
+      {/* calling component as a function */}
+      {Message()}
     </main>
   );
 }
-// Making Component
-const Greetings = (props:any)=>{
-  return(
-    <div>
-      <h2>Hello {props.name}! , Welcome to our coding world</h2>
-    </div>
-  )
-}
+
+
